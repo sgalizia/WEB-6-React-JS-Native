0dbd9905a4e342dab2af6db67589a998ab95be02
Sam Galizia <samgalizia@gmail.com>
2017-09-12 11:39:44 -0700

    Added homework for week 1

---
+++ week-1-challenges.js
@@ -0,0 +1,58 @@
+// Week 1 Challenges
+
+function Person(name, title) {
+  this.name = name
+  this.title = title
+};
+
+var person = new Person("Sam", "Student");
+console.log(person);
+
+console.log("------------------------\n");
+
+var nameTitle = [];
+
+var person2 = new Person("Brandon Stark", "the Three-Eyed Raven");
+var person3 = new Person("Jon Snow", "the King in the North");
+var person4 = new Person("Cersei Lannister", "Queen of the Andals and the First Men");
+var person5 = new Person("Danerys Targaryen", "the Mother of Dragons");
+
+nameTitle.push(person);
+nameTitle.push(person2);
+nameTitle.push(person3);
+nameTitle.push(person4);
+nameTitle.push(person5);
+
+console.log(nameTitle);
+
+console.log("------------------------\n");
+
+function onlyNamesWith(letter) {
+  return nameTitle.filter( function(person) {
+    return person.name.charAt(0).toLowerCase() === letter.toLowerCase();
+  });
+};
+
+console.log(onlyNamesWith("s"));
+
+console.log("------------------------\n");
+
+function matchingTitle(title) {
+  return nameTitle.filter( function(person) {
+    return person.title.toLowerCase() === title.toLowerCase();
+  });
+};
+
+console.log(matchingTitle("the Mother of Dragons"));
+
+console.log("------------------------\n");
+
+function combineNameAndTitle() {
+  return nameTitle.map( function(person) {
+    return person.name + " " + person.title;
+  });
+}
+
+console.log(combineNameAndTitle());
+
+console.log("------------------------\n");
